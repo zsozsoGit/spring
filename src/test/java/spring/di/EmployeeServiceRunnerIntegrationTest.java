8748b617cc15e5dd9a85b2a4d637b607237e5dae
zsozsoGit <q30dCNmiaZQBjnf5Klzb>
2018-12-12 15:02:48 +0100

    Finish IntegrationTesting

---
+++ src/test/java/spring/di/EmployeeServiceRunnerIntegrationTest.java
@@ -0,0 +1,25 @@
+package spring.di;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
+public class EmployeeServiceRunnerIntegrationTest {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Test
+    public void testSaveList() {
+        employeeService.saveEmployee("    DDD ");
+        assertEquals(List.of("DDD"), employeeService.listEmployees());
+    }
+}
