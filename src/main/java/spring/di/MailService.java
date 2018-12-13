800e8a849b1190793bb51aeb8497bcd9f8e01964
Training <zs>
2018-12-13 13:15:00 +0100

    Property handling++

--- src/main/java/spring/di/MailService.java
+++ src/main/java/spring/di/MailService.java
@@ -1,11 +1,16 @@
 package spring.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MailService {
+    @Autowired
+    private Environment environment;
     public void sendMail()
     {
         System.out.println("sending mail");
+        System.out.println("Env sender"+ environment.getProperty("OS"));
     }
 }
