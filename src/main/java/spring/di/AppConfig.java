5a889598da6120ca95ff3d2d4f8104aa146baa0f
zsozsoGit <q30dCNmiaZQBjnf5Klzb>
2018-12-14 10:07:28 +0100

    Revert "First Testing"
    This reverts commit 7824882cdf1506532141f1bd2b367cfa897c6866.
    + JDBC easy + test

--- src/main/java/spring/di/AppConfig.java
+++ src/main/java/spring/di/AppConfig.java
@@ -1,21 +1,27 @@
 package spring.di;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackageClasses = AppConfig.class)
+@PropertySource("classpath:/application.properties")
 public class AppConfig {
+    @Autowired
+    private Environment environment;
     @Bean
     public DataSource dataSource() {
         MysqlDataSource mysqlDataSource = new MysqlDataSource();
-        mysqlDataSource.setUrl("jdbc:mysql://localhost:3306/employees?useUnicode=true");
-        mysqlDataSource.setUser("employees");
-        mysqlDataSource.setPassword("employees");
+        mysqlDataSource.setUrl(environment.getProperty("jdbc.url"));
+        mysqlDataSource.setUser(environment.getProperty("jdbc.username"));
+        mysqlDataSource.setPassword(environment.getProperty("jdbc.password"));
         return mysqlDataSource;
 
     }
--- src/main/java/spring/di/EmployeeDao.java
+++ src/main/java/spring/di/EmployeeDao.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -19,10 +23,37 @@ public class EmployeeDao {
 
 
     public void saveEmployee(String name) {
-        employee.add(name);
+        try (
+                Connection conn = myDataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement("insert into employees(emp_name) values (?)")
+        ) {
+            ps.setString(1, name);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public List<String> listEmployees() {
-        return new ArrayList<>(employee);
+        try (
+                Connection conn = myDataSource.getConnection();
+                PreparedStatement ps = conn.prepareStatement("select emp_name from employees");
+                ResultSet rs = ps.executeQuery()
+        ) {
+            List<String> names = new ArrayList<>();
+            while (rs.next()) {
+                String name = rs.getString("emp_name");
+                names.add(name);
+
+            }
+            return names;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
